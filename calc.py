ddf21660d61bace4e43425b6cfc8da23d56243b3
SagarMD12194 <sagarmd888@gmail.com>
2020-05-28 20:43:37 +0530

    tests cases are added

--- calc.py
+++ calc.py
@@ -1,4 +1,16 @@
 class Calculator:
     def add_func(self,num1,num2):
+        print("Addition Function is Started...")
         return num1+num2
+
+
+def main():
+    calc=Calculator()
+    num1,num2=10,20
+    result=calc.add_func(num1,num2)
+    print("Addition of %d and %d = %d"%(num1,num2,result))
+
+if __name__ == '__main__':
+    main()
+
 
